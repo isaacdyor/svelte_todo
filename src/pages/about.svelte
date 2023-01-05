93d32d62918b9ebf6b500ef39cced0dd39a74aa7
Isaac <isaac@dyor.com>
2023-01-04 22:01:50 -0800

    fixing texbox focus error

--- src/pages/about.svelte
+++ src/pages/about.svelte
@@ -1,152 +1,155 @@
-<style>
-  .container {
-    max-width: 600px;
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  .title {
-    font-size: 2em;
-    font-weight: bold;
-    margin: 20px 0;
-  }
-
-  .new-todo {
-    display: flex;
-    align-items: center;
-    margin: 20px 0;
-  }
-
-  .input {
-    flex: 1;
-    font-size: 1em;
-    padding: 10px;
-    border: none;
-    border-radius: 5px 0 0 5px;
-  }
-
-  .button {
-    font-size: 1em;
-    padding: 10px;
-    border: none;
-    border-radius: 0 5px 5px 0;
-    background-color: #00b8d4;
-    color: white;
-    cursor: pointer;
-  }
-
-  .button:hover {
-    background-color: #0052cc;
-  }
-
-  .todo {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 10px 0;
-    padding: 10px;
-    background-color: white;
-    border-radius: 5px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  }
-
-  .todo input[type="checkbox"] {
-    margin-right: 10px;
-  }
-
-  .todo p {
-    flex: 1;
-    font-size: 1em;
-    margin: 0;
-    /* text-decoration: {task.complete ? 'line-through' : 'none'}; */
-  }
-
-  .todo button {
-    font-size: 0.8em;
-    padding: 5px 10px;
-    border: none;
-    border-radius: 5px;
-    background-color: #00b8d4;
-    color: white;
-    cursor: pointer;
-  }
-
-  .todo button:hover {
-    background-color: #0052cc;
-  }
-
-  .button-container {
-    display: flex;
-    justify-content: center;
-    margin: 20px 0;
-  }
-
-  .filter-button {
-    font-size: 1em;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: #00b8d4;
-    color: white;
-    cursor: pointer;
-    margin: 0 5px;
-  }
-</style>	
-
 <script>
+  let task = "";
+	let editTask = "";
+	let tasks = [];
+	let activeTasks = [];
+	let activeButton = "all";
+  function addTask(myTask) {
+    tasks.push(myTask);
+		tasks = tasks;
+    task = "";
+		updateTasks();
+  }
 	function filter(condition) {
-    activeButton = condition;
-    updateTasks();
+		activeButton = condition;
+		updateTasks();
   }
-
-  function updateTasks() {
-    if (activeButton === "all") {
-      activeTasks = tasks;
-    } else if (activeButton === "active") {
-      activeTasks = tasks.filter(task => !task.complete);
-    } else {
-      activeTasks = tasks.filter(task => task.complete);
-    }
-  }
-
-  function setCompletion(task) {
-    task.complete = !task.complete;
-    updateTasks();
-  }
-
-  function deleteTask(task) {
-    const index = tasks.indexOf(task);
-    tasks.splice(index, 1);
-    updateTasks();
-  }
-
-  class Todo {
-    constructor(value) {
-      this.value = value;
-      this.complete = false;
-      this.id = tasks.length;
-    }
-  }
+	function updateTasks() {
+		if (activeButton == "all") {
+			activeTasks = tasks
+		} else if (activeButton == "active") {
+			activeTasks = tasks.filter(task => task.complete == false)
+		} else {
+			activeTasks = tasks.filter(task => task.complete == true)
+		}	
+		console.log(activeTasks)	
+	}
+	function setCompletion(task) {
+		task.complete = !task.complete
+		updateTasks();
+	}
+	function deleteTask(task) {
+		let index = tasks.indexOf(task);
+		console.log(index);
+		tasks.splice(index, 1);
+		task.selected = "unselected";
+		updateTasks();
+	}
+	function cancel(task) {
+		task.value = task.temp;
+		task.selected = "unselected";
+		updateTasks();
+	}
+	class Todo {
+		constructor(value) {
+			this.value = value;
+			this.temp = value;
+			this.complete = false;
+			this.id = tasks.length;
+			this.selected = "unselected";
+		}
+	}
 </script>
 
 <div class="container">
   <h1 class="title">Todo list</h1>
-  <div class="new-todo">
-    <input class="input" bind:value={task}>
-    <button class="button" on:click={() => addTask(new Todo(task))}>Add</button>
-  </div>
-
-  <div class="button-container">
-    <button class:active={activeButton === "all"} on:click={() => filter("all")} class="filter-button">All</button>
-    <button class:active={activeButton === "active"} on:click={() => filter("active")} class="filter-button">Active</button>
-    <button class:active={activeButton === "completed"} on:click={() => filter("completed")} class="filter-button">Completed</button>
-  </div>
-
-  {#each activeTasks as task}
-    <div class="todo">
-      <input type="checkbox" on:change={() => setCompletion(task)} checked={task.complete} />
-      <p>{task.value}</p>
-      <button on:click={() => deleteTask(task)} class="button">Delete</button>
-    </div>
-  {/each}
+	<div class="new-todo">
+		<input class="input" bind:value={task}>
+		<button class="button" on:click={addTask(new Todo(task))}>Add</button>
 </div>
+
+<div class="button-container">
+	<button class:active={activeButton === "all"} on:click={() => filter("all")} class="filter-button">All</button>
+	<button class:active={activeButton === "active"} on:click={() => filter("active")} class="filter-button">Active</button>
+	<button class:active={activeButton === "completed"} on:click={() => filter("completed")} class="filter-button">Completed</button>
+</div>
+
+{#key activeTasks}
+	{#each activeTasks as task}
+	<div>
+		<label>
+			{#if task.selected == "unselected"}
+				<input type="checkbox" on:change={() => setCompletion(task)} checked = {task.complete}/>
+				{task.value} 
+				<br>
+				<button on:click={() => task.selected = "edit"} class="edit-button">Edit</button>
+				<button on:click={() => task.selected = "delete"} class="delete-button">Delete</button>
+			{:else if task.selected == "edit"}
+				<input class="input" bind:value={task.value} >
+				<br>
+				<button on:click={() => task.selected = "unselected"} class="edit-button">Save</button>
+				<button on:click={() => cancel(task)} class="delete-button">Cancel</button>
+			{:else}
+				<input type="checkbox" on:change={() => setCompletion(task)} checked = {task.complete}/>
+				{task.value} 
+				<br>
+				<p>Are you sure you want to delete this todo?</p>
+				<br>
+				<button on:click={() => deleteTask(task)} class="edit-button">Delete</button>
+				<button on:click={() => task.selected = "unselected"} class="delete-button">Cancel</button>
+			{/if}
+
+
+		</label>
+	</div>
+	{:else}
+	<p>No todos!</p>	
+	{/each}
+{/key}
+
+</div>
+
+
+
+<style>
+  .container {
+    text-align: center;
+    width: 50%;
+    border: 3px solid #0000ff;
+    transform: translate(50%, 10%)
+  }
+  .input {
+    padding: 0.75rem 1.25rem;
+  }
+  .button {
+    border: none;
+    background-color: #007bff;
+    color: white;
+    padding: 0.75rem 1.25rem;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+  input[type="checkbox"] {
+    width: 1.2em;
+    height: 1.2	em;
+  }
+	label {
+    margin-left: 0.5em;
+    font-size: 1.2em;
+  }
+	.button-container {
+		text-align: center;
+  }
+  .filter-button {
+    color: #007bff;
+    border: 2px solid #007bff;
+    background-color: white;
+  }
+  .active {
+    background-color: #007bff;
+    color: white;
+  }
+	.edit-button {
+    color: #007bff;
+    border: 2px solid #007bff;
+    background-color: white;
+  }
+	.delete-button {
+    background-color: #007bff;
+		border: 2px solid #007bff;
+    color: white;
+  }
+	.new-todo {
+		padding: 20px;
+	}
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,175 +1,233 @@
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .title {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    color: #007bff;
+  }
+
+  .new-todo {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  .input {
+    font-size: 1rem;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .button {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    border: 1px solid #007bff;
+    border-radius: 4px;
+    background-color: #007bff;
+    color: #fff;
+    cursor: pointer;
+  }
+	.button:hover {
+    background-color: #0069d9;
+  }
+
+  .button-container {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+
+  .filter-button {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: #fff;
+    cursor: pointer;
+    margin-right: 0.5rem;
+  }
+
+  .filter-button.active {
+    background-color: #007bff;
+    color: #fff;
+  }
+
+	.edit-button,
+	.delete-button {
+		width: 100px;
+		height: 30px;
+		font-size: 1rem;
+		padding: 0;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		background-color: #fff;
+		cursor: pointer;
+		margin-left: 1rem;
+		position: relative;
+		top: -2px;
+	}
+
+  .edit-button:hover,
+  .delete-button:hover {
+    background-color: #ddd;
+  }
+
+	label {
+		display: flex;
+		align-items: center;
+		font-size: 1.1rem;
+		margin-bottom: 0.5rem;
+		line-height: 30px;
+		padding: 0rem 0;
+	}
+
+
+	input[type="checkbox"] {
+		width: 20px;
+		height: 20px;
+		margin-right: 0.5rem;
+		position: relative;
+		top: -2px;
+	}
+
+	.filter-button:not(.active):hover {
+		background-color: #ddd;
+	}
+
+	.check-label {
+		position: relative;
+		top: -5px;
+	}
+
+</style>
+
 <script>
   let task = "";
-	let editTask = "";
-	let tasks = [];
-	let activeTasks = [];
-	let activeButton = "all";
+  let tasks = [];
+  let activeTasks = [];
+  let activeButton = "all";
+
 
   function addTask(myTask) {
     tasks.push(myTask);
-		tasks = tasks;
+    tasks = tasks;
     task = "";
-		updateTasks();
+    updateTasks();
   }
 
-	function filter(condition) {
-		activeButton = condition;
-		updateTasks();
+  function filter(condition) {
+    activeButton = condition;
+    updateTasks();
   }
 
-	function updateTasks() {
-		if (activeButton == "all") {
-			activeTasks = tasks
-		} else if (activeButton == "active") {
-			activeTasks = tasks.filter(task => task.complete == false)
-		} else {
-			activeTasks = tasks.filter(task => task.complete == true)
-		}	
-		console.log(activeTasks)	
-	}
+  function updateTasks() {
+    if (activeButton == "all") {
+      activeTasks = tasks;
+    } else if (activeButton == "active") {
+      activeTasks = tasks.filter((task) => task.complete == false);
+    } else {
+      activeTasks = tasks.filter((task) => task.complete == true);
+    }
+    console.log(activeTasks);
+  }
 
-	function setCompletion(task) {
-		task.complete = !task.complete
-		updateTasks();
-	}
+  function setCompletion(task) {
+    task.complete = !task.complete;
+    updateTasks();
+  }
 
+  function deleteTask(task) {
+    let index = tasks.indexOf(task);
+    console.log(index);
+    tasks.splice(index, 1);
+    task.selected = "unselected";
+    updateTasks();
+  }
 
-	function deleteTask(task) {
-		let index = tasks.indexOf(task);
-		console.log(index);
-		tasks.splice(index, 1);
-		task.selected = "unselected";
-		updateTasks();
-	}
+  function cancel(task) {
+    task.value = task.temp;
+    task.selected = "unselected";
+    updateTasks();
+  }
 
-	function cancel(task) {
-		task.value = task.temp;
-		task.selected = "unselected";
-		updateTasks();
-	}
-
+  function editTask(task) {
+    task.temp = task.value;
+    task.selected = "unselected";
+    updateTasks();
+  }
 	class Todo {
-		constructor(value) {
-			this.value = value;
-			this.temp = value;
-			this.complete = false;
-			this.id = tasks.length;
-			this.selected = "unselected";
-		}
-	}
+    constructor(value) {
+      this.value = value;
+      this.temp = value;
+      this.complete = false;
+      this.id = tasks.length;
+      this.selected = "unselected";
+    }
+  }
 
 </script>
 
 <div class="container">
   <h1 class="title">Todo list</h1>
-	<div class="new-todo">
-		<input class="input" bind:value={task}>
-		<button class="button" on:click={addTask(new Todo(task))}>Add</button>
-</div>
+  <div class="new-todo">
+    <input class="input" bind:value={task}>
+    <button class="button" on:click={addTask(new Todo(task))}>Add</button>
+  </div>
 
-<div class="button-container">
-	<button class:active={activeButton === "all"} on:click={() => filter("all")} class="filter-button">All</button>
-	<button class:active={activeButton === "active"} on:click={() => filter("active")} class="filter-button">Active</button>
-	<button class:active={activeButton === "completed"} on:click={() => filter("completed")} class="filter-button">Completed</button>
-</div>
-
-{#key activeTasks}
+  <div class="button-container">
+    <button
+      class:active={activeButton === "all"}
+      on:click={() => filter("all")}
+      class="filter-button"
+    >
+      All
+    </button>
+    <button
+      class:active={activeButton === "active"}
+      on:click={() => filter("active")}
+      class="filter-button"
+    >
+      Active
+    </button>
+    <button
+      class:active={activeButton === "completed"}
+      on:click={() => filter("completed")}
+      class="filter-button"
+    >
+      Completed
+    </button>
+  </div>
+	{#key activeTasks}
 	{#each activeTasks as task}
-	<div>
-		<label>
-			{#if task.selected == "unselected"}
-				<input type="checkbox" on:change={() => setCompletion(task)} checked = {task.complete}/>
-				{task.value} 
-				<br>
-				<button on:click={() => task.selected = "edit"} class="edit-button">Edit</button>
-				<button on:click={() => task.selected = "delete"} class="delete-button">Delete</button>
-			{:else if task.selected == "edit"}
-				<input class="input" bind:value={task.value} >
-				<br>
-				<button on:click={() => task.selected = "unselected"} class="edit-button">Save</button>
-				<button on:click={() => cancel(task)} class="delete-button">Cancel</button>
-			{:else}
-				<input type="checkbox" on:change={() => setCompletion(task)} checked = {task.complete}/>
-				{task.value} 
-				<br>
-				<p>Are you sure you want to delete this todo?</p>
-				<br>
-				<button on:click={() => deleteTask(task)} class="edit-button">Delete</button>
-				<button on:click={() => task.selected = "unselected"} class="delete-button">Cancel</button>
-			{/if}
-
-
-		</label>
-	</div>
-	{:else}
-	<p>No todos!</p>	
+		<div>
+			<label>
+				{#if task.selected == "unselected"}
+					<input type="checkbox" on:change={() => setCompletion(task)} checked={task.complete} />
+					<p class="check-label">{task.value}</p>
+					<br>
+					<button on:click={() => task.selected = "edit"} class="edit-button">Edit</button>
+					<button on:click={() => task.selected = "delete"} class="delete-button">Delete</button>
+				{:else if task.selected == "edit"}
+				<input class="input" bind:value={task.value} autofocus/>
+					<br>
+					<button on:click={() => editTask(task)} class="edit-button">Save</button>
+					<button on:click={() => cancel(task)} class="delete-button">Cancel</button>
+				{:else}
+					<input type="checkbox" on:change={() => setCompletion(task)} checked={task.complete} />
+					{task.value}
+					<br>
+					<button on:click={() => deleteTask(task)} class="edit-button">Delete</button>
+					<button on:click={() => task.selected = "unselected"} class="delete-button">Cancel</button>
+				{/if}
+			</label>
+		</div>
+		{:else}
+			<p>No todos!</p>	
 	{/each}
 {/key}
-
 </div>
-
-
-
-<style>
-  .container {
-    text-align: center;
-    width: 50%;
-    border: 3px solid #0000ff;
-    transform: translate(50%, 10%)
-  }
-
-  .input {
-    padding: 0.75rem 1.25rem;
-  }
-
-  .button {
-    border: none;
-    background-color: #007bff;
-    color: white;
-    padding: 0.75rem 1.25rem;
-    font-size: 1rem;
-    cursor: pointer;
-  }
-
-  input[type="checkbox"] {
-    width: 1.2em;
-    height: 1.2	em;
-  }
-
-	label {
-    margin-left: 0.5em;
-    font-size: 1.2em;
-  }
-
-	.button-container {
-		text-align: center;
-  }
-
-  .filter-button {
-    color: #007bff;
-    border: 2px solid #007bff;
-    background-color: white;
-  }
-
-  .active {
-    background-color: #007bff;
-    color: white;
-  }
-
-	.edit-button {
-    color: #007bff;
-    border: 2px solid #007bff;
-    background-color: white;
-  }
-
-	.delete-button {
-    background-color: #007bff;
-		border: 2px solid #007bff;
-    color: white;
-  }
-
-	.new-todo {
-		padding: 20px;
-	}
-
-</style>
